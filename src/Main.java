c21607956b1c54eb9b72d2cccec9a13ca3346de9
njiddasalifu <njiddasalifu@gmail.com>
2023-06-22 23:16:12 +0100

    revamp in the main class

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ public class Main {
     //this is the main class for the project
     public static void main(String[] args) throws Exception {
         System.out.println("\n****** Hello, Welcome to our Library management System.******");
-       
-       
-        // admin.registerLibrarian();
-        // Login login = new Login();
-        // login.login();
-
         Login lg = new Login();
         lg.login();
        
